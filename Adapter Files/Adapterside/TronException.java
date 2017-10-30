3a874bf573a82054f2a24587f202389ae8080d1f
Apneet Kaur Sandhu <apneetsandhu88@gmail.com>
2017-10-30 09:39:52 +0200

    tronexception

---
+++ Adapter Files/Adapterside/TronException.java
@@ -0,0 +1,11 @@
+package com.uppaal.tron;
+
+import java.io.IOException;
+
+public class TronException extends IOException
+{
+    public TronException(String message)
+    {
+	super(message);
+    }
+}
